5ee41e7375411634cf6e8d594642ac8b47c2cc56
Yusuf Aslan <250541068@firat.edu.tr>
2025-12-03 21:41:30 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -17,5 +17,14 @@ public class Exercise4_4 {
         // System.out.println(sayHello() + 7);
 
         // Cevaplarinizi yorum olarak ekleyin.
+        
+
+Soru: Bir değer döndüren metodu çağırıp sonucunu kullanmazsanız ne olur?
+
+Cevap: Java'da bu hata vermez. Metot çalışır, işini yapar, dönen değer (return value) hafızada bir yere atanmadığı için kaybolur (ignore edilir).
+
+Soru: Bir void metodu bir ifadenin içinde kullanırsanız (örn: println() + 7) ne olur?
+
+Cevap: Java derleme hatası (compile-time error) verir. Çünkü void metotlar bir değer üretmez, dolayısıyla matematiksel veya mantıksal bir işleme sokulamazlar. Hatada genellikle "void type not allowed here" yazar.
     }
 }
