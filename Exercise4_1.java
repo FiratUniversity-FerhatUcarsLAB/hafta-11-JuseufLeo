3f11651ce83f7ae806991c997d05ad30f017188f
Yusuf Aslan <250541068@firat.edu.tr>
2025-12-03 21:39:45 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,27 @@
-public class Exercise4_1 {
-
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
-    }
-
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
-    }
+public class DatePrinter {
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        // Amerikan formatı testi
+        System.out.println("Amerikan Formatı:");
+        printAmerican(day, date, month, year);
+
+        // Avrupa formatı testi
+        System.out.println("Avrupa Formatı:");
+        printEuropean(day, date, month, year);
+    }
+
+    // Amerikan: "Monday, July 22, 2019"
+    public static void printAmerican(String day, int date, String month, int year) {
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+    }
+
+    // Avrupa: "22 July 2019, Monday"
+    public static void printEuropean(String day, int date, String month, int year) {
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 }
